a6a3ae924ffcca17afa49c5148f1dee7ad70ad98
Will Arachelian <william.arachelian@gmail.com>
2024-11-17 10:50:26 -0500

    set up

---
+++ electron-app/src/renderer/src/App.jsx
@@ -0,0 +1,16 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <p className="text-xl">Text</p>
+      </div>
+    </>
+  )
+}
+
+export default App
+
